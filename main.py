e5975f4d69329590caeb96eefc243d691a48855e
Nikora Muriwai <105256789+NikoraMuriwai@users.noreply.github.com>
2022-05-26 22:35:16 +0000

    #Welcome print ("Kia Ora, Ko Wai Tou Ingoa? / Hello, What Is Your Name") name = input() print ("Nau Mai Haere Mai / Welcome " + name)
    #Chances
    chances = 1
    print("You Will Have", chances ,"Chance To Answer Correctly")
    
    #Score
    score = 0
    
    #question 1
    question_1 = print("1) What Is Te Reo For Ocean?")
    options_1 = print("A) Moana | B) Rangi | C) Papa | D) Moko")
    answer_1 = "a"
    
    for i in range (chances):
      answer = input("Answer: ")
      if (answer.lower() == answer_1):
        print ("Kai Pai" +name)
        score = score + 1
        break
    else:
        print

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-print ("Kia Ora, Ko Wai Tou Ingoa? ")
+#Welcome 
+print ("Kia Ora, Ko Wai Tou Ingoa? / Hello, What Is Your Name")
 name = input()
-print ("Nau Mai Haere Mai " + name)
-# -------------------------
-def new_game():
+print ("Nau Mai Haere Mai / Welcome " + name)
 
-  question_prompt
+#Chances
+chances = 1
+print("You Will Have", chances ,"Chance To Answer Correctly")
+
+#Score
+score = 0
+
+#question 1
+question_1 = print("1) What Is Te Reo For Ocean?")
+options_1 = print("A) Moana | B) Rangi | C) Papa | D) Moko")
+answer_1 = "a"
+
+for i in range (chances):
+  answer = input("Answer: ")
+  if (answer.lower() == answer_1):
+    print ("Kai Pai" +name)
+    score = score + 1
+    break
+else:
+    print
